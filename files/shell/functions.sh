0e4ab35b58c70935a776eb193a334ec76406d6a2
Youri T. K. K. Mattar <youri@youhide.com.br>
2022-10-21 09:36:00 -0300

    add awsuse/awsclear

--- files/shell/functions.sh
+++ files/shell/functions.sh
@@ -1,4 +1,4 @@
-hideascii() {
+function hideascii() {
 echo ${GREEN}
 cat << "EOF"
   .__    .__    .___     ________          __   
@@ -11,6 +11,27 @@ EOF
 echo ${NOCOLOR}
 }
 
-yodaquote() {
+function yodaquote() {
   echo ${PURPLE}$(yoda-quotes)${NOCOLOR} 
+}
+
+function awsuse() {
+  if [ -z "$1" ]; then
+    echo "No environment supplied"
+  else
+    if grep -q "\[profile $1\]" ~/.aws/config; then
+      export AWS_PROFILE=${1}
+      echo "AWS command line environment set to [${1}]"
+    else
+      echo "AWS profile [${1}] not found."
+      echo "Please choose from an existing profile:"
+      grep "\[profile" ~/.aws/config
+      echo "Or create a new one."
+    fi
+  fi
+}
+
+function awsclear() {
+  unset AWS_PROFILE
+  echo "AWS command line environment cleared."
 }
