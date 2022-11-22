5414ad6c23b62d44f896401de22a1e48b10756d1
Youri T. K. K. Mattar <youri@youhide.com.br>
2022-11-22 13:54:29 -0300

    update

--- files/shell/imports.sh
+++
@@ -1,8 +0,0 @@
-source ~/.dotFiles/files/shell/oh-my-zsh.sh
-source ~/.dotFiles/files/shell/functions.sh
-source ~/.dotFiles/files/shell/aliases.sh
-source ~/.dotFiles/files/shell/exports.sh
-source ~/.dotFiles/files/shell/paths.sh
-source ~/.dotFiles/files/shell/completion.sh
-source ~/.dotFiles/files/shell/extra.sh
-source ~/.dotFiles/files/shell/colors.sh
