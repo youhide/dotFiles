de68355beeff7a2e36198c70d1183f8d59aae245
Youri T. K. K. Mattar <youri@youhide.com.br>
2023-05-25 15:53:41 -0300

    fix export order

---
+++ files/shell/5_exports.sh
@@ -0,0 +1,12 @@
+# Proxmox
+#PM_USER=$(pass hideOut/Terraform/Proxmox/user)
+#PM_PASS=$(pass hideOut/Terraform/Proxmox/pass)
+
+# Terraform
+export TF_VAR_proxmox_api_token_id=$(pass hideOut/Terraform/Proxmox/token_id)
+export TF_VAR_proxmox_api_token_secret=$(pass hideOut/Terraform/Proxmox/token_secret)
+
+# Fortis Games
+export NEXUS_USER=$(pass Fortis/Nexus/user)
+export NEXUS_PASS=$(pass Fortis/Nexus/pass)
+export NPM_TOKEN=$(pass Fortis/Nexus/NPM_Token)
