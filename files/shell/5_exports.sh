36922df24539cfede5e44410c8516fac5a8d89e1
Youri T. K. K. Mattar <youri@youhide.com.br>
2023-05-26 15:12:47 -0300

    Update 5_exports.sh
    add TF_TOKEN

--- files/shell/5_exports.sh
+++ files/shell/5_exports.sh
@@ -3,6 +3,7 @@
 #PM_PASS=$(pass hideOut/Terraform/Proxmox/pass)
 
 # Terraform
+export TF_TOKEN_app_terraform_io=$(pass hideOut/Terraform/app_terraform_io)
 export TF_VAR_proxmox_api_token_id=$(pass hideOut/Terraform/Proxmox/token_id)
 export TF_VAR_proxmox_api_token_secret=$(pass hideOut/Terraform/Proxmox/token_secret)
 
