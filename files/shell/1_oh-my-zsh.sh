e5fd3ee9d62706e748976db1a8ad2e7dd0475fab
youhide <youri@youhide.com.br>
2024-02-07 12:11:35 -0300

    Add terraform and ansible plugins to oh-my-zsh configuration

--- files/shell/1_oh-my-zsh.sh
+++ files/shell/1_oh-my-zsh.sh
@@ -71,7 +71,7 @@ ZSH_THEME="robbyrussell"
 # Custom plugins may be added to $ZSH_CUSTOM/plugins/
 # Example format: plugins=(rails git textmate ruby lighthouse)
 # Add wisely, as too many plugins slow down shell startup.
-plugins=(git kubectl)
+plugins=(git kubectl terraform ansible)
 
 # User configuration
 
