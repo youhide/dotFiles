4822cda9685f835a3446be092840815592d64333
Youri T. K. K. Mattar <youri@youhide.com.br>
2024-02-19 23:02:55 -0300

    Update completion scripts for tofu and terragrunt

--- files/shell/6_completion.sh
+++ files/shell/6_completion.sh
@@ -10,4 +10,5 @@ source $(brew --prefix)/share/zsh-autosuggestions/zsh-autosuggestions.zsh
 
 autoload -U +X bashcompinit && bashcompinit
 complete -o nospace -C $(brew --prefix)/bin/terraform terraform
-complete -o nospace -C /usr/local/bin/tofu tofu
+complete -o nospace -C $(brew --prefix)/bin/tofu tofu
+complete -o nospace -C $(brew --prefix)/bin/terragrunt terragrunt
