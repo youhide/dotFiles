53650d1bf8395e27abdd1ea7fbe6cb4a192d95f5
Youri T. K. K. Mattar <youri@youhide.com.br>
2024-03-01 01:25:06 -0300

    Update brew alias to include autoremove

--- files/shell/3_aliases.sh
+++ files/shell/3_aliases.sh
@@ -1,4 +1,4 @@
 # UPDATE
-alias update-brew="brew update && brew upgrade && brew cleanup"
+alias update-brew="brew update && brew upgrade && brew autoremove && brew cleanup"
 alias update-dnf="sudo dnf update -y && sudo dnf autoremove"
 alias update-apt="sudo apt update && sudo apt upgrade -y && sudo apt autoremove"
