69f4fc528d587e040fc12bbbdba169045447b0c0
youhide <youri@youhide.com.br>
2025-05-03 01:37:37 -0300

    updt

--- files/shell/5_exports.sh
+++ files/shell/5_exports.sh
@@ -5,8 +5,9 @@ if which pass >/dev/null; then
     #PM_PASS=$(pass hideOut/Terraform/Proxmox/pass)
 
     # Terraform
-    # export TF_TOKEN_app_terraform_io=$(pass hideOut/Terraform/app_terraform_io)
-    # export TF_VAR_proxmox_api_token_id=$(pass hideOut/Terraform/Proxmox/token_id)
-    # export TF_VAR_proxmox_api_token_secret=$(pass hideOut/Terraform/Proxmox/token_secret)
+    export TF_VAR_proxmox_token_id=$(pass hideOut/Terraform/Proxmox/token_id)
+    export TF_VAR_proxmox_token_secret=$(pass hideOut/Terraform/Proxmox/token_secret)
+    export TF_VAR_s3_access_key=$(pass hideOut/OpenMediaVault/s3/access_key)
+    export TF_VAR_s3_secret_key=$(pass hideOut/OpenMediaVault/s3/secret_key)
 
 fi
