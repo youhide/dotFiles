f834613da3cf59c8f9dc0b3ed4603408c3d69ce4
youhide <youri@youhide.com.br>
2025-05-04 17:17:56 -0300

    Refactor environment variable exports for Proxmox in 5_exports.sh

--- files/shell/5_exports.sh
+++ files/shell/5_exports.sh
@@ -5,8 +5,12 @@ if which pass >/dev/null; then
     #PM_PASS=$(pass hideOut/Terraform/Proxmox/pass)
 
     # Terraform
-    export TF_VAR_proxmox_token_id=$(pass hideOut/Terraform/Proxmox/token_id)
-    export TF_VAR_proxmox_token_secret=$(pass hideOut/Terraform/Proxmox/token_secret)
+    export PM_API_TOKEN_ID=$(pass hideOut/Terraform/Proxmox/token_id)
+    export PM_API_TOKEN_SECRET=$(pass hideOut/Terraform/Proxmox/token_secret)
+
+    export MINIO_USER=$(pass hideOut/OpenMediaVault/s3/user)
+    export MINIO_PASSWORD=$(pass hideOut/OpenMediaVault/s3/pass)   
+     
     export TF_VAR_s3_access_key=$(pass hideOut/OpenMediaVault/s3/access_key)
     export TF_VAR_s3_secret_key=$(pass hideOut/OpenMediaVault/s3/secret_key)
 
