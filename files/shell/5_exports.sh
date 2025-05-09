5e203d9ca95e6f2c9159f541754a7dd52d837867
Youri T. K. K. Mattar <youri@youhide.com.br>
2025-05-09 14:55:22 -0300

    Remove commented Proxmox and Terraform lines; ensure VAULT_ADDR is exported in 5_exports.sh

--- files/shell/5_exports.sh
+++ files/shell/5_exports.sh
@@ -1,10 +1,8 @@
 if which pass >/dev/null; then
 
-    # Proxmox
     #PM_USER=$(pass hideOut/Terraform/Proxmox/user)
     #PM_PASS=$(pass hideOut/Terraform/Proxmox/pass)
 
-    # Terraform
     export PM_API_TOKEN_ID=$(pass hideOut/Terraform/Proxmox/token_id)
     export PM_API_TOKEN_SECRET=$(pass hideOut/Terraform/Proxmox/token_secret)
 
@@ -16,5 +14,6 @@ if which pass >/dev/null; then
 
     export VAULT_TOKEN=$(pass hideOut/Vault/root_token)
     export VAULT_UNSEAL_KEY=$(pass show hideOut/Vault/unseal_key)
+    export VAULT_ADDR=$(pass show hideOut/Vault/addr)
 
 fi
